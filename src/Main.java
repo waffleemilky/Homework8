e70a3cc4ddffa351483e335cf1681c3e4adb962d
waffleemilky <6sun6light6@gmail.com>
2025-01-22 00:30:33 +0300

    Homework8 Fix

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,32 +16,62 @@ public class Main {
 
 //         Task 2
 
-        System.out.print(a[0] + ", " + a[1] + ", " + a[2]);
-        System.out.println();
-        System.out.print(b[0] + ", " + b[1] + ", " + b[2]);
-        System.out.println();
-        System.out.print(c[0]);
-        System.out.println();
-
-//        Task 3
-
-        System.out.print(a[2] + ", " + a[1] + ", " + a[0]);
-        System.out.println();
-        System.out.print(b[2] + ", " + b[1] + ", " + b[0]);
-        System.out.println();
-        System.out.print(c[0]);
-        System.out.println();
-
-//        Task 4
-
         for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 != 0)
-                a[i] += 1;
             if (i < a.length - 1)
                 System.out.print(a[i] + ", ");
             else {
                 System.out.print(a[i]);
             }
         }
+        System.out.println();
+            for (int i = 0; i < b.length; i++) {
+                if (i < b.length - 1)
+                    System.out.print(b[i] + ", ");
+                else {
+                    System.out.print(b[i]);
+                }
+            }
+        System.out.println();
+            for (int i = 0; i < c.length; i++) {
+                if (i < c.length - 1)
+                    System.out.print(c[i] + ", ");
+                else {
+                    System.out.print(c[i]);
+                }
+            }
+        System.out.println();
+
+//        Task 3
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.print(a[i]);
+            if (i != 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+        for (int i = b.length - 1; i >= 0; i--) {
+            System.out.print(b[i]);
+            if (i != 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+        for (int i = c.length - 1; i >= 0; i--) {
+            System.out.print(c[i]);
+            if (i != 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+//        Task 4
+
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] % 2 != 0)
+                    a[i] += 1;
+                if (i < a.length - 1)
+                    System.out.print(a[i] + ", ");
+                else {
+                    System.out.print(a[i]);
+                }
+            }
+        }
     }
-}
